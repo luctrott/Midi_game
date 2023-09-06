4d6a981a10fa6bf23d48c269516d4c7868aae9fb
luctrott <120565362+luctrott@users.noreply.github.com>
2023-09-06 11:52:56 +0200

    Create Logic_Settings.py

---
+++ Source_code/Logic_Settings.py
@@ -0,0 +1,11 @@
+class Logic_Settings:
+    #LCD
+    cols = 20
+    rows = 4
+    charmap = 'A00'
+    i2c_expander = 'PCF8574'
+    address = 0x27 
+    port = 1
+
+    #MIDI
+    midi_port = 'Microsoft GS Wavetable Synth 0'
