f63897d202257d2c71d6399d755f603987e49541
luctrott <120565362+luctrott@users.noreply.github.com>
2023-09-07 08:57:12 +0200

    Added setup for folders

---
+++ Source_code/setup.py
@@ -0,0 +1,14 @@
+from LogicSettings import LogicSettings
+import os
+
+def __mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def create_folder():
+    __mkdir(LogicSettings.dev_dir)
+    __mkdir(LogicSettings.usb_dir)
+    __mkdir(LogicSettings.onboard_dir)
+if __name__ == "__main__":
+    create_folder()
+    print("Done")
