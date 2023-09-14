71872fb681f94239a421a13156d7f64e5de43d81
luctrott <120565362+luctrott@users.noreply.github.com>
2023-09-14 10:42:53 +0200

    Tests

--- Source_code/flügel.py
+++ Source_code/flügel.py
@@ -1,6 +1,7 @@
 from LCDHandler import LCDHandler
 from CustomChars import CustomChars
 from FileManager import FileManager
+import time
 
 class main:
     def __init__(self,game) -> None:
@@ -14,8 +15,25 @@ class main:
         self.__lcd.create_char(4,CustomChars.char4)
         self.__lcd.create_char(5,CustomChars.char5)
         self.__lcd.create_char(6,CustomChars.char6)
+        time.sleep(1)
         self.__progress_bar_chars=['\x01','\x02','\x03','\x04']
-        
+        self.__progress=0
+        self.__progress_bar_curent_chars=[]
+        self.__draw_progress_bar(1)
+    
+    def __draw_progress_bar(self,progress:int)->None:
+        if progress != self.__progress:
+            self.__progress=progress
+            tmp=[]
+            self.__lcd.cursor_pos=(0,1)
+            for char in self.__progress_bar_chars:
+                self.__lcd.write_string(char)
+                
+
+                
+                    
+
+            
     def close(self) -> None:
         self.__lcd.close()
         self.__filemanager.close()
