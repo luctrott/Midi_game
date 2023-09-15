87beb8e2c8efcd2cfb63f723ee0eb2c2a3604c7a
luctrott <120565362+luctrott@users.noreply.github.com>
2023-09-15 17:51:29 +0200

    outsourceing frame and add volume

---
+++ Source_code/Frames.py
@@ -0,0 +1,58 @@
+from LCDHandler import LCDHandler
+from RuntimeVariables import RuntimeVariables
+class Frames:
+    def __init__(self, lcd:LCDHandler):
+        self.__lcd=lcd
+        self.__last_volume=-1
+    
+    def __center_text(self,text:str,fill:str=" ",lenth:int=20)->str:
+        if len(text)<lenth:
+            text=fill*int((lenth-len(text))/2)+text+fill*int((lenth-len(text))/2)
+        if len(text)<lenth:
+            text=text+fill
+        return text
+        
+    def _draw_frame(self, text:str=None, incrementel:bool=True)->None:
+        self.__lcd.cursor_pos=(0,0)
+        if text is None:
+            self.__lcd.write_string("-"*20)
+        else:
+            self.__lcd.write_string(self.__center_text(text,fill="-"))
+        if not incrementel:
+            self.__lcd.cursor_pos=(0,3)
+            self.__lcd.write_string("-"*20)
+            self.__lcd.cursor_pos=(0,1)
+            self.__lcd.write_string("|")
+            self.__lcd.cursor_pos=(19,1)
+            self.__lcd.write_string("|")
+            self.__lcd.cursor_pos=(0,2)
+            self.__lcd.write_string("|")
+            self.__lcd.cursor_pos=(19,2)
+            self.__lcd.write_string("|")
+    
+    def volume(self, volume:int)->None:
+        if not RuntimeVariables.screen==10:
+            if RuntimeVariables.screen>10:
+                self._draw_frame(incrementel=True)  
+            else:
+                self._draw_frame(incrementel=False)
+            self.__lcd.cursor_pos=(1,1)
+            self.__lcd.write_string(self.__center_text("Volume:",lenth=18))
+        
+        if (not self.__last_volume==volume) or RuntimeVariables.screen!=10:
+            if  RuntimeVariables.screen!=10:
+                self.__lcd.cursor_pos=(1,2)
+                self.__lcd.write_string(self.__center_text(f"{volume}%",lenth=18))
+            else:
+                #overwrite the old three digits only
+                self.__lcd.cursor_pos=(8,2)
+                self.__lcd.write_string(self.__center_text(f"{volume}%",lenth=4,fill="="))
+
+
+            self.__last_volume=volume
+            RuntimeVariables.screen=10
+            if callable(RuntimeVariables.event_callback):
+                RuntimeVariables.event_callback()
+
+        
+        
--- Source_code/flügel.py
+++ Source_code/flügel.py
@@ -2,6 +2,7 @@ from LCDHandler import LCDHandler
 from CustomChars import CustomChars
 from FileManager import FileManager
 from ProgressBar import ProgressBar
+from Frames import Frames
 
 import time
 
@@ -18,34 +19,10 @@ class main:
         self.__lcd.create_char(5,CustomChars.char5)
         self.__lcd.create_char(6,CustomChars.char6)
         self.__proggres_bar=ProgressBar(self.__lcd)
+        self.__frames=Frames(self.__lcd)
         self.__closed=False
         self.__proggres_bar.force_reload()
-        self.__draw_frame(incrementel=False)
-    
-    def __center_text(self,text:str,fill:str=" ",lenth:int=20)->str:
-        if len(text)<lenth:
-            text=fill*int((lenth-len(text))/2)+text+fill*int((lenth-len(text))/2)
-        if len(text)<lenth:
-            text=text+fill
-        return text
         
-    def __draw_frame(self, text:str=None, incrementel:bool=True)->None:
-        self.__lcd.cursor_pos=(0,0)
-        if text is None:
-            self.__lcd.write_string("-"*20)
-        else:
-            self.__lcd.write_string(self.__center_text(text,fill="-"))
-        if not incrementel:
-            self.__lcd.cursor_pos=(0,3)
-            self.__lcd.write_string("-"*20)
-            self.__lcd.cursor_pos=(0,1)
-            self.__lcd.write_string("|")
-            self.__lcd.cursor_pos=(19,1)
-            self.__lcd.write_string("|")
-            self.__lcd.cursor_pos=(0,2)
-            self.__lcd.write_string("|")
-            self.__lcd.cursor_pos=(19,2)
-            self.__lcd.write_string("|")
 
     def close(self) -> None:
         self.__lcd.close()
@@ -55,5 +32,5 @@ class main:
     def run_logic(self)->None:
 
         while self.__closed==False:
-            time.sleep(1)
+            self.__frames.volume(int(input("volume: ")))
     
